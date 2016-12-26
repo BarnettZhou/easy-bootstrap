dc502591cbcf73796370825de0dbd1482440b3fe
zhouxuchen <zhouxuchen1993@foxmail.com>
2016-12-26 16:01:52 +0800

    combobox: dropdown-size; btn-size;

--- combobox.easy-bootstrap.js
+++ combobox.easy-bootstrap.js
@@ -4,6 +4,7 @@
     var Combobox = function(args, jqObject) {
         this._defaults = {
             width: 0,               // 组件宽度，单位px
+            btnSize: 'default',     // 按钮大小，lg、sm、xs或留空（default）
             valueField: 'value',    // 值的字段
             textField: 'text',      // 显示文字的字段
             data: [],               // 本地加载的数据
@@ -16,6 +17,13 @@
         this._selectedRow   = {};
         this._value_record  = {};
         this.init(args, jqObject);
+    }
+
+    var _dropdown_option = {
+        'default':  { caret_margin: '10%',  font_size: '14px', },
+        'lg':       { caret_margin: '10%',  font_size: '14px', },
+        'sm':       { caret_margin: '7%',   font_size: '12px', },
+        'xs':       { caret_margin: '7%',   font_size: '12px', },
     }
 
     /**
@@ -32,25 +40,33 @@
      * 插入dom，绑定数据与事件
      */
     Combobox.prototype.insertComboboxBody = function(jqObject) {
-        var _this           = this;
-
         var data            = this._options.data;
         var valueField      = this._options.valueField;
         var textField       = this._options.textField;
 
         this._base_style = '';
         if (this._options.width) {
-            this._base_style += 'width:' + String(this._options.width) + 'px;';
+            this._base_style += 'min-width:' + String(this._options.width) + 'px;';
         }
+
+        // 按钮大小，默认default
+        if (!this._options.btnSize) { this._options.btnSize = 'default'; }
 
         var _jq_ul = this.composeDropdownList();
 
         // 按钮部分
-        var _btn_html  = '<a class="btn btn-default dropdown-toggle" type="button" id="'+this._btn_id+'" data-toggle="dropdown" style="'+this._base_style+'height:34px;">';
+        var _btn_class = 'btn btn-default dropdown-toggle';
+        if (this._options.btnSize && this._options.btnSize != 'default') {
+            _btn_class += ' btn-' + this._options.btnSize;
+        }
+        var _btn_html  = '<a class="'+_btn_class+'" type="button" id="'+this._btn_id+'" data-toggle="dropdown" style="'+this._base_style+'">';
             _btn_html += '<span class="dropdown-text pull-left">'+this._selectedRow[textField];
-            _btn_html += '</span><span class="caret pull-right" style="margin-top:10%;"></span></a>';
+            _btn_html += '</span><span class="caret pull-right" style="margin-top:'+_dropdown_option[this._options.btnSize]['caret_margin']+';"></span></a>';
 
-        jqObject.attr('hidden', 'true').wrap('<div class="dropdown" />').before(_btn_html).before(_jq_ul);
+        jqObject.attr('hidden', 'true')
+            .wrap('<div class="dropdown" />')
+            .before(_btn_html)
+            .before(_jq_ul);
     }
 
     /**
@@ -77,7 +93,11 @@
 
         for (key in data) {
             this._value_record[data[key][this._options.valueField]] = data[key];
-            var _jq_li = $('<li role="presentation"><a role="menuitem" tabindex="-1" href="#">'+data[key][this._options.textField]+'</a></li>');
+            var _li_html  = '<li role="presentation">';
+                _li_html += '<a role="menuitem" tabindex="-1" href="#" ';
+                _li_html += 'style="font-size:'+_dropdown_option[this._options.btnSize]['font_size']+';">';
+                _li_html += data[key][this._options.textField]+'</a></li>';
+            var _jq_li = $(_li_html);
             _jq_li.data('record', data[key])    // 绑定数据
                 .on('click', function() {       // 绑定点击事件
                     $('#'+_this._btn_id + ' > .dropdown-text').html($(this).data('record')[_this._options.textField]);
