6c0b0749c72299595b83b0af90ff19426c330836
zhouxuchen <zhouxuchen1993@foxmail.com>
2016-12-26 17:22:40 +0800

    btn width full

--- combobox.easy-bootstrap.js
+++ combobox.easy-bootstrap.js
@@ -11,19 +11,20 @@
             onSelect: function(record) {},
         };
 
-        this._btn_id        = '_eb_combobox_btn_' + this.flag;
-        this._ul_id         = '_eb_combobox_ul_' + this.flag;
-        this._base_style    = '';
-        this._selectedRow   = {};
-        this._value_record  = {};
+        this._btnId        = '_eb_combobox_btn_' + this.flag;
+        this._ulId         = '_eb_combobox_ul_' + this.flag;
+        this._baseStyle    = '';
+        this._selectedRow  = {};
+        this._valueRecord  = {};
         this.init(args, jqObject);
     }
 
     var _dropdown_option = {
-        'default':  { caret_margin: '10%',  font_size: '14px', },
-        'lg':       { caret_margin: '10%',  font_size: '14px', },
-        'sm':       { caret_margin: '7%',   font_size: '12px', },
-        'xs':       { caret_margin: '7%',   font_size: '12px', },
+        'default':  { caret_margin: '10px',  font_size: '14px', },
+        'lg':       { caret_margin: '10px',  font_size: '14px', },
+        'sm':       { caret_margin: '8px',   font_size: '12px', },
+        'xs':       { caret_margin: '6px',   font_size: '12px', },
+        'full':     { caret_margin: '1%',   font_size: '14px', },
     }
 
     /**
@@ -43,25 +44,30 @@
         var data            = this._options.data;
         var valueField      = this._options.valueField;
         var textField       = this._options.textField;
+        var btnSize         = this._options.btnSize;
 
-        this._base_style = '';
-        if (this._options.width) {
-            this._base_style += 'min-width:' + String(this._options.width) + 'px;';
+        this._baseStyle = '';
+        // 组件宽度为100%，下拉框设置及按钮宽度都设置为100%
+        if (this._options.width == 'full' || !this._options.width) {
+            this._baseStyle += 'min-width:100%;';
+            if (true) {}
+        } else {
+            this._baseStyle += 'min-width:' + String(this._options.width) + 'px;';
         }
-
-        // 按钮大小，默认default
-        if (!this._options.btnSize) { this._options.btnSize = 'default'; }
 
         var _jq_ul = this.composeDropdownList();
 
         // 按钮部分
         var _btn_class = 'btn btn-default dropdown-toggle';
-        if (this._options.btnSize && this._options.btnSize != 'default') {
-            _btn_class += ' btn-' + this._options.btnSize;
+        // 按钮大小
+        if (btnSize && btnSize != 'default') {
+            _btn_class += ' btn-' + btnSize;
+        } else {
+            btnSize = 'default';
         }
-        var _btn_html  = '<a class="'+_btn_class+'" type="button" id="'+this._btn_id+'" data-toggle="dropdown" style="'+this._base_style+'">';
+        var _btn_html  = '<a class="'+_btn_class+'" type="button" id="'+this._btnId+'" data-toggle="dropdown" style="'+this._baseStyle+'">';
             _btn_html += '<span class="dropdown-text pull-left">'+this._selectedRow[textField];
-            _btn_html += '</span><span class="caret pull-right" style="margin-top:'+_dropdown_option[this._options.btnSize]['caret_margin']+';"></span></a>';
+            _btn_html += '</span><span class="caret pull-right" style="margin-top:'+_dropdown_option[btnSize]['caret_margin']+';"></span></a>';
 
         jqObject.attr('hidden', 'true')
             .wrap('<div class="dropdown" />')
@@ -86,13 +92,13 @@
         }
 
         if (data.length) {
-            var _jq_ul = $('<ul class="dropdown-menu" role="menu" aria-labelledby="'+this._btn_id+'" id="'+this._ul_id+'" style="'+this._base_style+'"></ul>');
+            var _jq_ul = $('<ul class="dropdown-menu" role="menu" aria-labelledby="'+this._btnId+'" id="'+this._ulId+'" style="'+this._baseStyle+'"></ul>');
         } else {
             var _jq_ul = '';
         }
 
         for (key in data) {
-            this._value_record[data[key][this._options.valueField]] = data[key];
+            this._valueRecord[data[key][this._options.valueField]] = data[key];
             var _li_html  = '<li role="presentation">';
                 _li_html += '<a role="menuitem" tabindex="-1" href="#" ';
                 _li_html += 'style="font-size:'+_dropdown_option[this._options.btnSize]['font_size']+';">';
@@ -100,7 +106,7 @@
             var _jq_li = $(_li_html);
             _jq_li.data('record', data[key])    // 绑定数据
                 .on('click', function() {       // 绑定点击事件
-                    $('#'+_this._btn_id + ' > .dropdown-text').html($(this).data('record')[_this._options.textField]);
+                    $('#'+_this._btnId + ' > .dropdown-text').html($(this).data('record')[_this._options.textField]);
                     _this._selectedRow = $(this).data('record');
                     // 用户设置的回调
                     _this._options.onSelect($(this).data('record'));
@@ -120,10 +126,10 @@
      */
     Combobox.prototype.loadData = function(data, jqObject) {
         this._options.data = data;
-        $('#'+this._ul_id).remove();
+        $('#'+this._ulId).remove();
 
         var _jq_ul = this.composeDropdownList();
-        $('#'+this._btn_id+'>.dropdown-text')
+        $('#'+this._btnId+'>.dropdown-text')
             .html(this._selectedRow[this._options.textField])
             .parent().after(_jq_ul);
 
@@ -141,9 +147,9 @@
      * 设置Combobox的值
      */
     Combobox.prototype.setValue = function(value, jqObject) {
-        this._selectedRow = this._value_record[value];
+        this._selectedRow = this._valueRecord[value];
         var _text = this._selectedRow[this._options.textField];
-        $('#'+this._btn_id+' > .dropdown-text').html(_text);
+        $('#'+this._btnId+' > .dropdown-text').html(_text);
 
         return jqObject;
     }
